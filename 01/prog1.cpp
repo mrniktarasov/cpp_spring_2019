bb926de5da47ceeafae4f180808059c5a11380a2
Nikita Tarasov <48064171+mrniktarasov@users.noreply.github.com>
2019-03-20 15:38:13 +0300

    Update prog1.cpp

--- 01/prog1.cpp
+++ 01/prog1.cpp
@@ -1,89 +1,79 @@
 #include <iostream>
 #include <cstdlib>
-#include <cmath>
 #include "numbers.dat"
 
-int search (int soughtFor);
-int primeCount (int start, int end);
-int isPrime(const int pr);
+int search ( int soughtFor );
+int primeCount ( int start, int end );
+int isPrime ( const int pr );
 
 
-int main(int argc, char* argv[]){
-	int* range = new int [argc];
+int main ( int argc, char* argv[] ){
 	int result;
 	int start;
 	int end;
 	
-	if(argc >= 2)
+	if( argc >= 2 && (argc - 1) % 2 == 0 )
 	{
-		for (int i = 1; i < argc; ++i)
+		for ( int i = 1; i < argc; i++ ) 
 		{
-			range[i] = std::atoi(argv[i]);
-		}
-		if ((argc - 1)%2 == 0)
-	        for (int i = 1; i < argc; i++) 
-	        {
-				start = search ((range [i]));
-				end = search (range[++i]);
-				if (start == - 1 || end == - 1)
-					return - 1;
-				result = primeCount(start, end);
-				std::cout<<result<<std::endl;
-			} 
-		else
-			return - 1;
+			start = search ( std::atoi(argv[i]) );
+			end = search ( std::atoi(argv[++i]) );
+			if (start == - 1 || end == - 1)
+				return - 1;
+			result = primeCount ( start, end );
+			std::cout<<result<<std::endl;
+		} 
 		return 0;
-	}	else 
+	} else 
 		return - 1;
-	delete [] range;
 }
 
-int	search	(int soughtFor) {
+int	search ( int soughtFor ) {
 	int beg = 0;
 	int end = Size;
 	int middle;
 	
-	while (beg <= end)
+	while ( beg <= end )
 	{
-		middle = beg + (end - beg)/2;
-		if (soughtFor < Data[middle])
+		middle = beg + ( end - beg ) / 2;
+		if ( soughtFor < Data[middle] )
 			end = middle - 1;
-		else if (soughtFor > Data[middle])	
-			beg =  middle + 1;
+		else if ( soughtFor > Data[middle] )	
+			beg = middle + 1;
 		else
 			return middle;
 	}
 	return - 1;
 }
 
-int primeCount (int start, int end){
+int primeCount	( int start, int end ){
 	int counter = 0;
 	int temp;
 	
-	for ( int i = start; i <= end; i++)
+	for ( int i = start; i <= end; i++ )
 	{
-		if (isPrime(Data[i]))
+		if ( isPrime ( Data[i]) )
 		{
 			counter++;
 			temp = i;
 			i++;
-			while (Data[temp] == Data[i])
+			while ( Data[temp] == Data[i] )
 			{
 				counter++;
 				i++;
 			}
-		i--;	
-		temp = 0;	
+			i--;		
 		}
 	}
 	return counter;	
 }
 
-int isPrime(const int pr){
-	if (pr == 1)
+int isPrime ( const int pr ){
+	if ( pr == 1 )
 		return 0;
-	for(int i = 2; i*i <= pr; i++)
+	for( int i = 2; i*i <= pr; i++ )
 		if (pr % i == 0)
 			return 0;	
-	return 1;	
+	return	1;	
 }
+
